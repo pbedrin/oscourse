c15810aded585f681ef3ed7f1fcf54e873d3235f
pbedrin <pbedrin@mail.ru>
2022-12-27 19:43:55 +0300

    Lab 10 fix

--- kern/syscall.c
+++ kern/syscall.c
@@ -422,6 +422,8 @@ syscall(uintptr_t syscallno, uintptr_t a1, uintptr_t a2, uintptr_t a3, uintptr_t
         return sys_ipc_try_send((envid_t)a1, (uint32_t)a2, a3,(size_t)a4,(int)a5);
     } else if (syscallno == SYS_ipc_recv) {
         return sys_ipc_recv(a1, a2);
+    } else if (syscallno == SYS_region_refs) {
+        return sys_region_refs(a1, (size_t)a2, a3, a4);
     } else {
         return -E_NO_SYS;
     }
